bff73b138d6a643501eabfcea8f012d0fa47ddd9
Jair0508 <jairmm0508@gmail.com>
2025-03-24 23:30:17 -0500

    technologies component ready and create navbar component

--- jair-moreno/src/app/technologies/technologies.component.ts
+++ jair-moreno/src/app/technologies/technologies.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 @Component({
   selector: 'app-technologies',
   imports: [],
   templateUrl: './technologies.component.html',
-  styleUrl: './technologies.component.css'
+  styleUrl: './technologies.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class TechnologiesComponent {
+
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper', {
+      modules: [Navigation, Pagination],
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      loop: true,
+      allowTouchMove: true,
+      touchEventsTarget: 'container',
+    });
+  }
 
 }
