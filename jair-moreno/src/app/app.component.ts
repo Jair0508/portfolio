e7723c4aa1c9a2ea9920c8b3a6645a8d4f8ee3f3
Jair0508 <jairmm0508@gmail.com>
2025-03-26 13:23:23 -0500

    technologies component ready and experience component created

--- jair-moreno/src/app/app.component.ts
+++ jair-moreno/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { TechnologiesComponent } from "./technologies/technologies.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { ExperienceComponent } from "./experience/experience.component";
 
 @Component({
   selector: 'app-root',
-  imports: [AboutMeComponent, TechnologiesComponent],
+  imports: [
+    AboutMeComponent,
+    TechnologiesComponent,
+    NavbarComponent,
+    ExperienceComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
 }
