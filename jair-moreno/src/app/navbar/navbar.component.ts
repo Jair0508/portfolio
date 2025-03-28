890bfaf7734cd111d0132c5849567073374061c1
Jair0508 <jairmm0508@gmail.com>
2025-03-27 22:17:33 -0500

    Navbar scroll functionality ready

--- jair-moreno/src/app/navbar/navbar.component.ts
+++ jair-moreno/src/app/navbar/navbar.component.ts
@@ -1,18 +1,46 @@
 import { Component } from '@angular/core';
+import { trigger, style, transition, animate } from "@angular/animations";
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+  animations: [
+    trigger('menuAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0.8)' }),
+        animate('200ms cubic-bezier(0.68, -0.55, 0.27, 1.55)', 
+          style({ opacity: 1, transform: 'scale(1)' }))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in-out', 
+          style({ opacity: 0, transform: 'scale(0.8)' }))
+      ])
+    ]),
+  ],
 })
 export class NavbarComponent {
 
   isMenuOpen = false;
 
   toggleMenu(): void {
-    console.log('isMenuOpen', this.isMenuOpen);
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  scrollTo(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      const offset = 56;
+      const elementPosition = element.getBoundingClientRect().top + window.pageYOffset; // Posición del componente
+      const offsetPosition = elementPosition - offset;
+  
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+    this.isMenuOpen = false;
   }
 
 }
